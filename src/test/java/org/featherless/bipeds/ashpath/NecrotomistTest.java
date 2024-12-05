c227d185fd15907b1c76ac365715ca81b06485f1
joaoTorpe <victotorpe10@gmail.com>
2024-12-05 20:18:33 -0300

    feat: Necrotomist tests fixes #5

--- src/test/java/org/featherless/bipeds/ashpath/NecrotomistTest.java
+++ src/test/java/org/featherless/bipeds/ashpath/NecrotomistTest.java
@@ -4,9 +4,13 @@
  */
 package org.featherless.bipeds.ashpath;
 
+import org.featherless.bipeds.ashpath.utils.TestHelper;
 import org.featherlessbipeds.ashpath.entity.CremationQueue;
 import org.featherlessbipeds.ashpath.entity.Deceased;
 import org.featherlessbipeds.ashpath.entity.Necrotomist;
+import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -33,10 +37,12 @@ public void persistindo_necrotomista(){
     
     em.persist(necrotomist);
     em.flush();
+    
     Necrotomist persistido = em.find(Necrotomist.class, 2L);
     
     assertNotNull(necrotomist.getId()); 
     assertNotNull(persistido);
+    assertEquals("Autopsy",persistido.getSpecialization());
     assertEquals(1,persistido.getCremationQueueSet().size());
     assertEquals(1,persistido.getDeceasedSet().size());
 }
@@ -46,10 +52,26 @@ public void buscando_necrotomista(){
     Necrotomist necrotomist_buscado = em.find(Necrotomist.class, 1L);
     
     assertNotNull(necrotomist_buscado); 
-    assertEquals(1,necrotomist_buscado.getCremationQueueSet().size());
-    assertEquals(1,necrotomist_buscado.getDeceasedSet().size());
- 
+    assertEquals(2,necrotomist_buscado.getCremationQueueSet().size());
+    assertEquals(3,necrotomist_buscado.getDeceasedSet().size());
+    
+    necrotomist_buscado.getCremationQueueSet().forEach( queue ->{
+            assertThat(queue.getId().toString(), CoreMatchers.anyOf(equalTo("1"),equalTo("2")));
+    });
+    
+    necrotomist_buscado.getDeceasedSet().forEach( dec ->{
+            assertThat(dec.getName(), CoreMatchers.anyOf(
+                    equalTo("Helmuth Voss"),
+                    equalTo("Josef Baumler"),
+                    equalTo("Kroner Hass")            
+            
+            ));
+    
+    });
+    
+    
 
+    
 
     
     
