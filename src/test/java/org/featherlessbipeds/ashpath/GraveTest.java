1f975b25acc3e130bbb9a53ca31503cce0753f06
albertalvin8080 <albertalvin99@gmail.com>
2024-12-07 21:46:40 -0300

    chore: add GraveTest to main branch

---
+++ src/test/java/org/featherlessbipeds/ashpath/GraveTest.java
@@ -0,0 +1,38 @@
+package org.featherlessbipeds.ashpath;
+
+import org.featherlessbipeds.ashpath.entity.Grave;
+import org.featherlessbipeds.ashpath.utils.TestHelper;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+
+public class GraveTest extends TestHelper {
+    @Test
+    public void persistGrave_AddsEntityToDb_WhenSuccessful() {
+        // Criando uma nova instância de Grave
+        Grave grave = new Grave();
+        grave.setLocation("Row12, Plot 5");
+
+        // Persistindo a instância no banco
+        em.persist(grave);
+        em.flush();
+
+        // Verificando se o ID foi gerado corretamente
+        assertNotNull(grave.getId());
+
+        // Recuperando a instância persistida para validação
+        Grave persistedGrave = em.find(Grave.class, grave.getId());
+        assertEquals(persistedGrave.getLocation(), grave.getLocation());
+    }
+
+    @Test
+    public void findGrave_ReturnsEntity_WhenSuccessful() {
+        // Buscando uma instância já existente no banco de dados
+        Grave grave = em.find(Grave.class, 1L); // ID previamente conhecido do dataset
+        assertNotNull(grave);
+
+        // Verificando se os dados persistidos são os esperados
+        assertEquals(grave.getLocation(), "Frankfurt"); // Valor do dataset XML
+    }
+}
--- src/test/java/org/featherlessbipeds/ashpath/utils/TestSuite.java
+++ src/test/java/org/featherlessbipeds/ashpath/utils/TestSuite.java
@@ -4,6 +4,7 @@ import org.featherlessbipeds.ashpath.AdminTest;
 import org.featherlessbipeds.ashpath.CremationQueueTest;
 import org.featherlessbipeds.ashpath.DeathRegistrarTest;
 import org.featherlessbipeds.ashpath.DeceasedTest;
+import org.featherlessbipeds.ashpath.GraveTest;
 import org.featherlessbipeds.ashpath.NecrotomistTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -11,7 +12,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
 {
-    AdminTest.class, DeathRegistrarTest.class, NecrotomistTest.class, CremationQueueTest.class, DeceasedTest.class
+    AdminTest.class, DeathRegistrarTest.class, NecrotomistTest.class, CremationQueueTest.class, DeceasedTest.class, GraveTest.class
 })
 public class TestSuite
 {
