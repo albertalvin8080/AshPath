191a2431d57f9380e404428a72cf6bef45e9ff72
Albert Alvin <121321650+albertalvin8080@users.noreply.github.com>
2024-12-13 20:10:36 -0300

    Update TestHelper.java

--- src/test/java/org/featherlessbipeds/ashpath/utils/TestHelper.java
+++ src/test/java/org/featherlessbipeds/ashpath/utils/TestHelper.java
@@ -42,6 +42,8 @@ public abstract class TestHelper
         if (!et.getRollbackOnly())
         {
             et.commit();
+        } else {
+            et.rollback();
         }
         em.close();
     }
