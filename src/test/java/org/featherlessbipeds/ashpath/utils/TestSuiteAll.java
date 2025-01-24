406574abcd93129accf808f501d7451f11b0ef4c
Albert Alvin <121321650+albertalvin8080@users.noreply.github.com>
2025-01-24 20:06:46 -0300

    add testall

---
+++ src/test/java/org/featherlessbipeds/ashpath/utils/TestSuiteAll.java
@@ -0,0 +1,25 @@
+package org.featherlessbipeds.ashpath.utils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+{
+    org.featherlessbipeds.ashpath.first_task.AdminTest.class, 
+    org.featherlessbipeds.ashpath.first_task.DeathRegistrarTest.class, 
+    org.featherlessbipeds.ashpath.first_task.NecrotomistTest.class, 
+    org.featherlessbipeds.ashpath.first_task.CremationQueueTest.class, 
+    org.featherlessbipeds.ashpath.first_task.DeceasedTest.class, 
+    org.featherlessbipeds.ashpath.first_task.GraveTest.class,
+    org.featherlessbipeds.ashpath.second_task.AdminTest.class, 
+    org.featherlessbipeds.ashpath.second_task.DeathRegistrarTest.class, 
+    org.featherlessbipeds.ashpath.second_task.NecrotomistTest.class, 
+    org.featherlessbipeds.ashpath.second_task.CremationQueueTest.class, 
+    org.featherlessbipeds.ashpath.second_task.DeceasedTest.class, 
+    org.featherlessbipeds.ashpath.second_task.GraveTest.class,
+})
+public class TestSuiteAll {
+    
+}
